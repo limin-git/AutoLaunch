164781cdd2633805ceb473da62ffb38b2f8ca8ae
zhulimin <zhulimin.china@gmail.com>
2016-05-12 21:22:48 +0800

    AutoLaunch.bat

---
+++ AutoLaunch - Copy.bat
@@ -0,0 +1,30 @@
+@ECHO OFF
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+SET process_name=%1%
+SET batch_file=%2%
+SET /A wait_time=%3% + 1
+
+
+IF "%process_name%" == "" (
+	ECHO error: process name not specified.
+	GOTO :EOF
+)
+
+
+IF NOT EXIST %batch_file% (
+	ECHO error: batch file '%batch_file%' does not exist.
+	GOTO :EOF
+)
+
+
+:LOOP
+	TASKLIST | FIND /I "%process_name%" >NUL
+	IF %ERRORLEVEL% EQU 0 GOTO OK
+	PING localhost /n 2 >NUL
+GOTO LOOP
+
+
+:OK
+PING localhost /n !wait_time! >NUL
+CALL !batch_file!
--- AutoLaunch.bat
+++ AutoLaunch.bat
@@ -4,26 +4,25 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 SET process_name=%1%
 SET batch_file=%2%
 SET /A wait_time=%3% + 1
+SET has_error=false
 
-IF "!process_name!" == "" (
-	ECHO error: process name not specified.
+IF "%process_name%" == "" SET has_error=true &SET error_msg_1=process-name not specified.
+IF NOT EXIST !batch_file! SET has_error=true &SET error_msg_2=batch-file '!batch_file!' does not exist.
+
+IF "%has_error%" == "true " (
+	IF NOT "%error_msg_1%" == "" ECHO error: %error_msg_1%
+	IF NOT "%error_msg_2%" == "" ECHO error: %error_msg_2%
 	GOTO :EOF
 )
 
 
-IF NOT EXIST !batch_file! (
-	ECHO error: batch file '!batch_file!' does not exist.
-	GOTO :EOF
-)
-
-
-:loop
-	TASKLIST | FIND /I "!process_name!" >NUL
-	IF %ERRORLEVEL% EQU 0 GOTO ok
+:LOOP
+	TASKLIST | FIND /I "%process_name%" >NUL
+	IF %ERRORLEVEL% EQU 0 GOTO OK
 	PING localhost /n 2 >NUL
-GOTO loop
+GOTO LOOP
 
 
-:ok
+:OK
 PING localhost /n !wait_time! >NUL
-CALL !batch_file!
+CALL %batch_file%
